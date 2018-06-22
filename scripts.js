1cb8fe6d32008f44b5ab58840d15aa8749e2cc99
jillianmaher <jillian.maher2@boone.kyschools.us>
2018-06-22 15:24:14 -0400

    Updated storage of dates in the Firebase

--- scripts.js
+++ scripts.js
@@ -30,12 +30,13 @@ document.addEventListener('DOMContentLoaded', function() {
         var newResource = {
             description: document.getElementById("description").value, //Put the item description here
             qty: document.getElementById("qty").value, //Put the qty of items here 
+			maxQty: document.getElementById("qty").value,
             name: document.getElementById("name").value, //Name of the item here
             ownerLocation: document.getElementById("location").value, //Location of owner here
             ownerName: document.getElementById("ownerName").value, //Name of owner here
 			borrower: "STILL AVAILABLE", //Name of borrower empty
-			checkOut: new Date(),
-			checkIn: new Date(),
+			checkOut: new Date().toString(),
+			checkIn: new Date().toString(),
         };
     
         resourceRef.push(newResource);
